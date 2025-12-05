a9a34a3df2067c066a23ab9ed23a256392ed7b5a
Pavel Ciugunov (EXIT) <chugunov.py@gmail.com>
2025-12-05 12:21:12 +0100

    creating architecture 1

--- app/main.py
+++ app/main.py
@@ -1,8 +1,18 @@
 from fastapi import FastAPI
 from app.api.v1 import auth, users, subscriptions
+from fastapi.middleware.cors import CORSMiddleware
+
 
 app = FastAPI(title="x8-network")
 
 app.include_router(auth.router)
 app.include_router(users.router)
 app.include_router(subscriptions.router)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],  # Vite dev server
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
